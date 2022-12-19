e12d00ac3a1959f90792e374834af8d886a03a8f
nimikevin <97411659+nimikevin@users.noreply.github.com>
2022-12-19 18:00:23 -0500

    crud json

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,40 +1,75 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Article from '../components/Article';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 
 const Blog = () => {
 
-    const [content ,setContent]=useState('')
-    const [error, setError]=useState(false)
+    const [content, setContent] = useState('')
+    const [error, setError] = useState(false)
+    const [bloData, setBlogData] = useState([])
+    const [author, setAuthor]=useState('')
 
-    const handleSubmit=(e)=>{
+    // on appel l'api
+
+    const getData = () => {
+        axios.get(" http://localhost:3004/articles").then((res) => setBlogData(res.data))
+    };
+
+
+    useEffect(() => {
+        getData();
+    }, []
+    )
+
+    const handleSubmit = (e) => {
         // on previent les comportement par defaut du formulaire
         e.preventDefault();
-        if(content.length<140){
-           setError(true)
-        }else{
+        if (content.length < 140) {
+            setError(true)
+        } else {
+            axios.post("http://localhost:3004/articles",{
+                author,
+                content,
+                date: Date.now()
+            })
+            setAuthor("")
+            setContent("")
             setError(false)
+
+           
         }
-     
+       
+        getData();
+        window.location.reload();
+
     };
     return (
         <div className='blog-container'>
-             <Logo/>
-             <Navigation/>
-             <h1>Blog</h1>
-             <form onSubmit={(e)=> handleSubmit(e)}>
-                <input type='text' placeholder='Nom' />
+            <Logo />
+            <Navigation />
+            <h1>Blog</h1>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input type='text' placeholder='Nom' onChange={(e)=> setAuthor(e.target.value)} value={author} />
                 <textarea
-                style={{border:error? "1px solid red": "1px solid #61dafb"}}
-                 placeholder='Message' onChangeCapture={(e)=> setContent(e.target.value)}></textarea>
-               {error &&  <p>Veuillez écrire un minimum de 140 caractere </p>} 
-                <input type='submit' value='Envoyer'/>
-                
-             </form>
+                    style={{ border: error ? "1px solid red" : "1px solid #61dafb" }}
+                    placeholder='Message' onChange={(e) => setContent(e.target.value)} value={content}></textarea>
+                {error && <p>Veuillez écrire un minimum de 140 caractere </p>}
+                <input type='submit' value='Envoyer' />
 
-             <ul></ul>
+            </form>
 
-            
+            <ul>
+                {bloData.
+                sort((a,b)=> b.date- a.date).
+                map((article) => (
+                <Article key={article.id} article={article}/>
+                    ))}
+
+            </ul>
+
+
         </div>
     );
 };
